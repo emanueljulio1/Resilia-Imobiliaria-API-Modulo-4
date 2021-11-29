1abdf345ec6484fc028c5f5b2c8f41901b909cf4
Rafael Zelli <rafazelli@gmail.com>
2021-11-29 17:29:26 -0300

    FEAT: Atualizando e deletando casas

--- api/controllers/CasaController.js
+++ api/controllers/CasaController.js
@@ -10,6 +10,7 @@ class CasaController {
         }
     }
 
+
     static async pegaUmaCasa(req, res){
         const { id } = req.params
         try{
@@ -22,11 +23,36 @@ class CasaController {
         }  
     }
 
+
     static async criaCasa(req, res) {
         const novaCasa = req.body
         try {
         const novaCasaCriada = await database.casa.create(novaCasa)
         return res.status(200).json(novaCasaCriada)
+        } catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+
+    static async atualizaCasa(req, res) {
+        const { id } = req.params   
+        const novasInfos = req.body
+        try {
+            await database.casa.update(novasInfos,{ where: { id: Number(id) }})
+            const casaAtualizada =  await database.casa.findOne({where: { id: Number(id) } })
+            return res.status(200).json(casaAtualizada)
+        }catch(error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+
+    static async apagaCasa(req, res){
+        const { id } = req.params   
+        try{
+            await database.casa.destroy({where: { id: Number(id) } })
+            return res.status(200).json({message: `id ${id} deletado`})
         } catch(error){
             return res.status(500).json(error.message)
         }
