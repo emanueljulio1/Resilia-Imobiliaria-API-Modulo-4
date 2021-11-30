f68922d58740cfa18099b46402b039e5982c920d
Joyce Lusia <joycep.lusia@gmail.com>
2021-11-30 12:30:17 -0300

    REFACT: Route Agendamento

--- api/routes/agendaRoute.js
+++ api/routes/agendaRoute.js
@@ -3,7 +3,7 @@ const AgendaController = require('../controllers/AgendaController')
 
 const router = Router()
 
-router.get('/agenda', Agendaontroller.pegaTodasAsAgendas)
+router.get('/agenda', AgendaController.pegaTodasAsAgendas)
 router.get('/agenda/:id', AgendaController.pegaUmAgendamento)
 router.post('/agenda', AgendaController.criaAgendamento)
 router.put('/agenda/:id', AgendaController.atualizaAgenda)
