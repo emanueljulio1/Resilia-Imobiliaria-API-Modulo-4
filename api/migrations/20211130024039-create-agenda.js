9952c08f80d166ec9e1dbabe89320ea4eceab9d7
Joyce Lusia <joycep.lusia@gmail.com>
2021-11-29 23:44:51 -0300

    FEAT: model agenda

---
+++ api/migrations/20211130024039-create-agenda.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('agendas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      data: {
+        type: Sequelize.DATE
+      },
+      codigo_imovel: {
+        type: Sequelize.STRING
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      cpf: {
+        type: Sequelize.STRING
+      },
+      telefone: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('agendas');
+  }
+};
