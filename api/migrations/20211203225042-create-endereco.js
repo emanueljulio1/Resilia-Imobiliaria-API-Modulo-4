c3aae8b40863f313ff075eb046373131e804e1b5
SrFrozi <fyrago@live.com>
2021-12-03 20:14:09 -0300

    Commit inserindo atualização de endereço

---
+++ api/migrations/20211203225042-create-endereco.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Enderecos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      numero: {
+        type: Sequelize.NUMBER
+      },
+      complemento: {
+        type: Sequelize.STRING
+      },
+      bairro: {
+        type: Sequelize.STRING
+      },
+      cep: {
+        type: Sequelize.NUMBER
+      },
+      cidade: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Enderecos');
+  }
+};
